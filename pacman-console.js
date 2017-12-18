e2342e72831e1ddf66b5f455fd370fc207b62adf
Omair <omairraza9@gmail.com>
2017-12-18 17:40:10 -0500

    done

---
+++ pacman-console.js
@@ -0,0 +1,15 @@
+// pacman-arrays.js
+
+var inky ={};
+
+inky.colour = 'cayan';
+inky.personality = 'Shadow';
+console.log(inky);
+
+// pacman-objects-2.js
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
+console.log(ghosts);
+
+
+
+// pacman-objects-2.js
